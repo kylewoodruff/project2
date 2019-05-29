63628f4ebcc3066cace10ae25671cb68770ef8bf
Kyle Woodruff <kylewoodruff@gmail.com>
2019-05-28 23:06:50 -0700

    updated config to use .env file

---
+++ config/config.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  "development": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "dialect": "mysql",
+    "logging": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+};
