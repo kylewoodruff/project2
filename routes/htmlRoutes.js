00c1506fbf013dda204aba4ec281359d3c5d7164
jxmiller5 <jxmiller5@gmail.com>
2019-06-01 12:58:40 -0700

    added launch to views

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");;
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./view"));
   });
 
   // Load example page and pass in an example by id
