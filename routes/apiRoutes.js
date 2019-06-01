bf8a510a4b18f73fa67924ad44d3749d1e93ee13
Electronbattle <40840029+Electronbattle@users.noreply.github.com>
2019-06-01 13:33:16 -0700

    Api data routes updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,91 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/subs", function(req, res) {
-    db.subs.findAll({}).then(function(dbsubs) {
-      res.json(dbsubs);
-    });
+  //all subs for user
+  app.get("/", function(req, res) {
+    db.subscription
+      .findAll({})
+      .then(function(result) {
+        var hbsObject = {
+          subscription: result
+        };
+        res.render("index", hbsObject);
+      })
+      .catch(function(err) {
+        res.status(500).send(err);
+      });
   });
-
-  // Create a new example
-  app.post("/api/subs", function(req, res) {
-    db.subs.create(req.body).then(function(dbsubs) {
-      res.json(dbsubs);
-    });
+  // add new
+  app.post("/add", function(req, res) {
+    db.subscription
+      .create({
+        subscriptionName: req.body.subscriptionName,
+        amount: req.body.amount,
+        dueDate: req.body.dueDate,
+        user: req.body.user,
+        category: req.body.category
+      })
+      .then(function(subscription) {
+        res.send(subscription, "Subscription submitted to db");
+      })
+      .catch(function(err) {
+        res.status(500).send(err);
+      });
   });
-
-  // Delete an example by id
-  app.delete("/api/subs/:id", function(req, res) {
-    db.subs.destroy({ where: { id: req.params.id } }).then(function(dbsubs) {
-      res.json(dbsubs);
-    });
+  //sequelize/express function for updating whether a book has been read
+  app.put("/update/:id", function(req, res) {
+    db.subscription
+      .update(
+        {
+          dueDate: req.body.dueDate
+        },
+        //   if(dueDate=== todayDate)
+        {
+          where: {
+            id: req.params.id
+          }
+        }
+      )
+      .then(function(result) {
+        res.json(result);
+      })
+      .catch(function(err) {
+        res.status(500).send(err);
+      });
+  });
+  //sequelize/express function for updating Amount of a subscription
+  app.put("/update-amount/:id", function(req, res) {
+    db.subscription
+      .update(
+        {
+          amount: req.body.amount
+        },
+        {
+          where: {
+            id: req.params.id
+          }
+        }
+      )
+      .then(function(result) {
+        res.json(result);
+      })
+      .catch(function(err) {
+        res.status(500).send(err);
+      });
+  });
+  //sequelize/express function for deleting a Subscription  from the model
+  app.delete("/delete/:id", function(req, res) {
+    db.subscription
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function() {
+        res.status(200).send();
+      })
+      .catch(function(err) {
+        res.status(500).send(err);
+      });
   });
 };
