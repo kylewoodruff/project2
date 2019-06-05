d1e91bf7fccade42fff73575b72f9600b080617c
Kyle Woodruff <kylewoodruff@gmail.com>
2019-06-04 21:30:44 -0700

    intial structure is built out

--- emails/subquery.js
+++ emails/subquery.js
@@ -1,15 +1,23 @@
-var db = require("../models");
+const db = require("../models");
+const Op = Sequelize.Op;
 
-module.exports = findDueSubs function() {
-    db.subscription.findAll({ include: [ subscription ] })
-    .then(function(res) {
-        if (res) {
-          res.status(200);
-          console.log("query successful");
-          
-        } else {
-          res.status(500);
-         console.log("query failed");
+let findSubsDue = function() {
+  db.subscription
+    .findAll({
+      where: {
+        dueDate: {
+          [Op.lte]: 7
         }
-      });
+      },
+      include: [users]
+    })
+    .then(function(res) {
+      if (res) {
+        res.status(200);
+        console.log("query successful");
+      } else {
+        res.status(500);
+        console.log("query failed");
+      }
+    });
 };
