0424c918dc3a930375ae28f0e69ca3ffc082e4d3
Kyle Woodruff <kylewoodruff@gmail.com>
2019-06-05 20:20:36 -0700

    Initial foundation for the db query

--- emails/subquery.js
+++ emails/subquery.js
@@ -1,6 +1,12 @@
 const db = require("../models");
 const Op = Sequelize.Op;
 
+// Use moment to get the current date (now).
+// Use moment to get the date 1 week from now.
+// Build loop for and Use moment to get the month/year of with the stored date.
+// build conditional to handle if day of month is 31/30 or 28th
+// build data call to check for subs inbetween Now and 1 week out from now.
+
 let findSubsDue = function() {
   db.subscription
     .findAll({
