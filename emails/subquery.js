a74f3f0422300db57d19e1cf29ef937dab9d22ca
Kyle Woodruff <kylewoodruff@gmail.com>
2019-06-05 21:59:41 -0700

    moment added and query built

--- emails/subquery.js
+++ emails/subquery.js
@@ -1,18 +1,28 @@
 const db = require("../models");
+var Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+const moment = require("moment");
 
-// Use moment to get the current date (now).
-// Use moment to get the date 1 week from now.
-// Build loop for and Use moment to get the month/year of with the stored date.
-// build conditional to handle if day of month is 31/30 or 28th
-// build data call to check for subs inbetween Now and 1 week out from now.
+let findSubs = function() {
+  // Use moment to get the current date (now).
+  let now = moment();
+  console.log(now, "Current date");
 
-let findSubsDue = function() {
+  // Use moment to get the date 1 week from now.
+  let futureDate = moment()
+    .add({ weeks: 1 })
+    .date();
+  console.log(futureDate, "future date");
+
+  // Build loop for and Use moment to get the month/year of with the stored date.
+  let dayOfMonth = now.date();
+  console.log(dayOfMonth);
   db.subscription
     .findAll({
       where: {
         dueDate: {
-          [Op.lte]: 7
+          // check for subs inbetween Now and 1 week out from now.
+          [Op.between]: [dayOfMonth, futureDate]
         }
       },
       include: [users]
@@ -21,9 +31,15 @@ let findSubsDue = function() {
       if (res) {
         res.status(200);
         console.log("query successful");
+        let data = res;
+        return data;
       } else {
         res.status(500);
         console.log("query failed");
       }
     });
 };
+
+findSubs();
+
+module.exports = findSubs;
