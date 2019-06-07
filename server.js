3c6ac19beafb1de87b369486b45b9be2f19aaaa5
jxmiller5 <jxmiller5@gmail.com>
2019-06-06 22:03:19 -0700

    changed some html

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -38,10 +37,7 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-
-
-
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
