e47c2222d7871d96f9050fe4c2b9d92a2ac4f25c
Kyle Woodruff <kylewoodruff@gmail.com>
2019-06-06 19:11:05 -0700

    paused point to merge master

--- emails/subquery.js
+++ emails/subquery.js
@@ -6,38 +6,35 @@ const moment = require("moment");
 let findSubs = function() {
   // Use moment to get the current date (now).
   let now = moment();
-  console.log(now, "Current date");
+  // console.log(now, "Current date");
 
   // Use moment to get the date 1 week from now.
   let futureDate = moment()
     .add({ weeks: 1 })
     .date();
-  console.log(futureDate, "future date");
+  // console.log(futureDate, "future date");
 
   // Build loop for and Use moment to get the month/year of with the stored date.
   let dayOfMonth = now.date();
-  console.log(dayOfMonth);
-  db.subscription
-    .findAll({
-      where: {
-        dueDate: {
-          // check for subs inbetween Now and 1 week out from now.
-          [Op.between]: [dayOfMonth, futureDate]
-        }
-      },
-      include: [users]
-    })
-    .then(function(res) {
-      if (res) {
-        res.status(200);
-        console.log("query successful");
-        let data = res;
-        return data;
-      } else {
-        res.status(500);
-        console.log("query failed");
-      }
-    });
+  // console.log(dayOfMonth);
+
+  let initData = [];
+
+  db.subscription.findAll({}).then(function(res) {
+    if (res) {
+      res.status(200);
+      console.log("query successful");
+      initData = res;
+      initData.forEach(i => {
+        
+        
+      });
+    } else {
+      res.status(500);
+      console.log("query failed");
+    }
+  });
+
 };
 
 findSubs();
