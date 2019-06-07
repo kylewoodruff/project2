fd5a804adf1a5f8666932f148bee65264c680680
George Shpak <georgeshpak@Georges-MacBook-Air.local>
2019-06-06 18:53:19 -0700

    Google Auth Completed

--- models/users.js
+++ models/users.js
@@ -1,5 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var users = sequelize.define("users", {
+    googleId: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
     firstName: {
       type: DataTypes.TEXT,
       allowNull: false
